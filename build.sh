5bc2e8caeb8f7988c4144620c55c8a7bc5cb225a
Michael Lesniak <mail@mlesniak.com>
2020-10-15 12:11:01 +0200

    Ignore build files

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env sh
 
+if [ ! -e aperol ]
+then
+    go build
+fi
+aperol
 git add .
 git commit -m "$(date)"
 git push
 
-if [ -e aperol ]
-then
-    aperol
-else
-    go run main.go
-fi
 
 rsync -rv docs/ server:/root/www
